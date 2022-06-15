7368b8e6524d859943b76638e890ad8ebe6a4384
YJE <primula374@naver.com>
2022-06-15 15:49:12 +0900

    220615

---
+++ js/function1.js
@@ -0,0 +1,22 @@
+function sum(n1, n2) {
+    return n1 + n2;
+}
+console.log(sum(10, 20));
+console.log(sum('10', '20')); //+
+
+function minus(v1, v2) {
+    return v1 - v2;
+}
+console.log(minus('20', '10'));
+
+function sumAry(ary) {
+    let sum = 0;
+    for (let arr of ary) {
+        sum += arr;
+    }
+    return sum;
+}
+
+let numAry = [20, 27, 33, 19, 45];
+let result = sumAry(numAry);
+console.log('배열의 합: ' + result);
